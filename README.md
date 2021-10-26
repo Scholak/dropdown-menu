f2ebba1b107de82138a39ad42e4657a1b143830d
Sebahattin Colak <scholaksebahattin@gmail.com>
2021-10-26 22:34:31 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Responsive Dropdown Menu
+## Simple animated accessible dropdown menu with Html, Css(scss) and Javascript
+![](/img/img1.png)
+![](/img/img2.png)
+# Note!
+## Refresh the page for mobile version (use tab and enter key for accessibility)
+![](/img/img3.png)
---
+++ js/app.js
@@ -0,0 +1,71 @@
+const burger = document.querySelector('.burger')
+const nav = document.querySelector('nav')
+const navItems = document.querySelectorAll('.nav-items')
+
+// it works only size of screen is smaller than 768px
+if(document.body.clientWidth < 768) {
+    
+    // navitagion toggle animation
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('active')
+        nav.classList.toggle('active')
+    })
+    burger.addEventListener('keypress', (e) => {
+        burger.classList.toggle('active')
+        e.code == 'Enter' ? nav.classList.toggle('active') : ''
+    })
+    
+    // sub menu animation
+    navItems.forEach(item => {
+        
+        item.addEventListener('click', () => {
+            
+            // removes active classes in the ul.sub-menu if exists 
+            navItems.forEach(item => {
+                item.classList.remove('active')
+                if(item.children[1]) {
+                    item.children[1].classList.remove('active')
+                    item.children[1].style.height = 0
+                }
+            })
+            
+            // sub menu toggle animation
+            if(item.children[1]) {
+                const length = item.children[1].children.length
+                
+                if(!item.children[1].classList.contains('active')) {
+                    item.classList.add('active')
+                    item.children[1].style.height = `${5 * length}rem`
+                } else {
+                    item.children[1].style.height = 0
+                }
+                item.children[1].classList.toggle('active')
+            }
+        })
+
+        item.addEventListener('focus', () => {
+            
+            // removes active classes in the ul.sub-menu if exists 
+            navItems.forEach(item => {
+                item.classList.remove('active')
+                if(item.children[1]) {
+                    item.children[1].classList.remove('active')
+                    item.children[1].style.height = 0
+                }
+            })
+            
+            // sub menu toggle animation
+            if(item.children[1]) {
+                const length = item.children[1].children.length
+                
+                if(!item.children[1].classList.contains('active')) {
+                    item.classList.add('active')
+                    item.children[1].style.height = `${5 * length}rem`
+                } else {
+                    item.children[1].style.height = 0
+                }
+                item.children[1].classList.toggle('active')
+            }
+        })
+    })
+}
